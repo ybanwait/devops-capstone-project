eba651e1d1ef600dbef5e027539a6767e50b8718
yadwinder <yadwinder.banwait@gmail.com>
2025-03-06 19:00:59 -0500

    fixed linting error

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -199,8 +199,8 @@ class TestAccountService(TestCase):
         headers = {
             'X-Frame-Options': 'SAMEORIGIN',
             'X-Content-Type-Options': 'nosniff',
-            'Content-Security-Policy': """default-src \'self\';
-            object-src \'none\'""",
+            'Content-Security-Policy':
+            'default-src \'self\';object-src \'none\'',
             'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
 
